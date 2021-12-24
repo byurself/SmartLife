5ebdb3294b6cbd00e3b206cdaf6d1d6fb7067499
byu_rself <1870287004@qq.com>
2021-12-24 22:48:02 +0800

    首页应用Fragment

--- app/src/main/java/com/lpc/smartlife/utils/Tools.java
+++ app/src/main/java/com/lpc/smartlife/utils/Tools.java
@@ -1,8 +1,12 @@
 package com.lpc.smartlife.utils;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.view.Gravity;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Tools {
 
@@ -10,5 +14,13 @@ public class Tools {
         Toast toast = Toast.makeText(context, msg, Toast.LENGTH_SHORT);
         toast.setGravity(Gravity.CENTER, 0, 0);
         toast.show();
+    }
+
+    public static Drawable makeDrawable(int id, AppCompatActivity a){
+        Resources resources = a.getApplicationContext().getResources();
+        Drawable drawable = resources.getDrawable(id);
+        //前两个参数为X轴和Y轴的起点,后两个参数是宽高
+        drawable.setBounds(1, 1, drawable.getMinimumHeight(), drawable.getMinimumWidth());
+        return drawable;
     }
 }
--- app/src/main/java/com/lpc/smartlife/views/smartlife/IndexActivity.java
+++ app/src/main/java/com/lpc/smartlife/views/smartlife/IndexActivity.java
@@ -1,24 +1,83 @@
 package com.lpc.smartlife.views.smartlife;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
-import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.hxc.basemodule.BaseActivity;
-import com.lpc.smartlife.MainActivity;
 import com.lpc.smartlife.R;
+import com.lpc.smartlife.utils.Tools;
+import com.lpc.smartlife.views.fragment.HomeFragment;
+import com.lpc.smartlife.views.fragment.UserCenterFragment;
 
 public class IndexActivity extends BaseActivity {
 
+    Fragment homeFragment;
+    Fragment userCenterFragment;
+    FragmentManager fragmentManager;
+    RadioGroup radioGroup;
+    RadioButton radioButtonHome;
+    RadioButton radioButtonMy;
+    ConstraintLayout conIndex;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
 
         setImmersiveWindows();
+        init();
+
     }
+
+    protected void init() {
+        homeFragment = new HomeFragment();
+        userCenterFragment = new UserCenterFragment();
+        fragmentManager = getSupportFragmentManager();
+
+        fragmentManager.beginTransaction().add(R.id.indexFrameLayout, homeFragment, "home").commit();
+        fragmentManager.beginTransaction().add(R.id.indexFrameLayout, userCenterFragment, "userCenter").commit();
+
+        fragmentManager.beginTransaction().hide(userCenterFragment).commit();
+
+        radioGroup=findViewById(R.id.indexRadioGroup);
+        radioButtonHome=findViewById(R.id.radioButtonHome);
+        radioButtonMy=findViewById(R.id.radioButtonMy);
+        conIndex=findViewById(R.id.con_index);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch (i){
+                    case R.id.radioButtonHome:
+                        conIndex.setBackground(getDrawable(R.mipmap.background));
+                        radioButtonHome.setCompoundDrawablesRelative(null, Tools.makeDrawable(R.mipmap.home_check,IndexActivity.this),null,null);
+                        radioButtonMy.setCompoundDrawablesRelative(null, Tools.makeDrawable(R.mipmap.my,IndexActivity.this),null,null);
+                        radioButtonHome.setTextColor(getResources().getColor(R.color.checked));
+                        radioButtonMy.setTextColor(getResources().getColor(R.color.black));
+                        break;
+                    case  R.id.radioButtonMy:
+                        conIndex.setBackground(getDrawable(R.drawable.background_white));
+                        radioButtonHome.setCompoundDrawablesRelative(null, Tools.makeDrawable(R.mipmap.home,IndexActivity.this),null,null);
+                        radioButtonMy.setCompoundDrawablesRelative(null, Tools.makeDrawable(R.mipmap.my_check,IndexActivity.this),null,null);
+                        radioButtonHome.setTextColor(getResources().getColor(R.color.black));
+                        radioButtonMy.setTextColor(getResources().getColor(R.color.checked));
+                        break;
+                }
+            }
+        });
+    }
+
+
+    public void ShowFragment(Fragment target) {
+        for (Fragment item : fragmentManager.getFragments()) {
+            fragmentManager.beginTransaction().hide(item).commit();
+        }
+        fragmentManager.beginTransaction().show(target).commit();
+    }
+
 
     @Override
     protected int getLayoutId() {
