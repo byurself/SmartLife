86bb0da00b5df567fb4bdcd409f30dc9d9b49b86
byu_rself <1870287004@qq.com>
2021-12-28 22:59:45 +0800

    设备管理显示房间列表

--- app/src/main/java/com/lpc/smartlife/views/smartlife/DeviceManagerActivity.java
+++ app/src/main/java/com/lpc/smartlife/views/smartlife/DeviceManagerActivity.java
@@ -1,15 +1,28 @@
 package com.lpc.smartlife.views.smartlife;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageButton;
 
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.hxc.basemodule.BaseActivity;
 import com.lpc.smartlife.R;
+import com.lpc.smartlife.adapter.RoomAdapter;
+import com.lpc.smartlife.entity.RoomList;
 
 public class DeviceManagerActivity extends BaseActivity {
 
     ImageButton ibBackToRoomInfo;
+
+    RecyclerView rvDeviceManager;
+    RoomAdapter roomAdapter;
+
+    View room;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +31,12 @@ public class DeviceManagerActivity extends BaseActivity {
 
         setImmersiveWindows();
         init();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        initRoomRecyclerView();
     }
 
     public void init(){
@@ -29,6 +48,24 @@ public class DeviceManagerActivity extends BaseActivity {
                 finish();
             }
         });
+
+        LayoutInflater inflater = getLayoutInflater().from(this);
+        room = inflater.inflate(R.layout.room_layout, null);
+    }
+
+    public void initRoomRecyclerView() {
+        rvDeviceManager = findViewById(R.id.rvDeviceManager);
+        // 布局设置
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        rvDeviceManager.setLayoutManager(manager);
+        // 设置适配器
+        RoomList.roomList.getRoomList();
+        roomAdapter = new RoomAdapter(this, RoomList.roomList.getRooms());
+        rvDeviceManager.setAdapter(roomAdapter);
+        // 设置分割线
+        rvDeviceManager.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        // 设置增加或删除条目的动画
+        rvDeviceManager.setItemAnimator(new DefaultItemAnimator());
     }
 
     @Override
