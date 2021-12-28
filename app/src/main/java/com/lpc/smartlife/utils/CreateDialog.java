52cea22e971e0b7ee5449606c1853ec791a8661f
byu_rself <1870287004@qq.com>
2021-12-28 17:53:11 +0800

    编辑家庭名称对话框

---
+++ app/src/main/java/com/lpc/smartlife/utils/CreateDialog.java
@@ -0,0 +1,71 @@
+package com.lpc.smartlife.utils;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.lpc.smartlife.R;
+
+/**
+ * @author byu_rself
+ * @date 2021/12/28 17:28
+ */
+public class CreateDialog extends Dialog {
+    /**
+     * 上下文对象 *
+     */
+    Activity context;
+
+    public Button bt_cancel;
+    public Button bt_confirm;
+    public EditText et_home_name;
+
+    private String roomName;
+    private int layout_id;
+    private View.OnClickListener mClickListener_cancel;
+    private View.OnClickListener mClickListener_confirm;
+
+
+    public CreateDialog(Activity context, int layout_id, int theme, View.OnClickListener clickListener_cancel, View.OnClickListener clickListener_confirm, String roonName) {
+        super(context, theme);
+        this.context = context;
+        this.mClickListener_cancel = clickListener_cancel;
+        this.mClickListener_confirm = clickListener_confirm;
+        this.roomName = roonName;
+        this.layout_id = layout_id;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.setContentView(layout_id);
+        /*
+         * 获取圣诞框的窗口对象及参数对象以修改对话框的布局设置, 可以直接调用getWindow(),表示获得这个Activity的Window
+         * 对象,这样这可以以同样的方式改变这个Activity的属性.
+         */
+        Window dialogWindow = this.getWindow();
+
+        WindowManager m = context.getWindowManager();
+        Display d = m.getDefaultDisplay(); // 获取屏幕宽、高用
+        WindowManager.LayoutParams p = dialogWindow.getAttributes(); // 获取对话框当前的参数值
+        // p.height = (int) (d.getHeight() * 0.6); // 高度设置为屏幕的0.6
+        p.width = (int) (d.getWidth() * 0.8); // 宽度设置为屏幕的0.8
+        dialogWindow.setAttributes(p);
+
+        // 根据id在布局中找到控件对象
+        bt_cancel = findViewById(R.id.bt_cancel);
+        bt_confirm = findViewById(R.id.bt_confirm);
+        et_home_name = findViewById(R.id.et_home_name);
+        this.et_home_name.setText(roomName);
+        // 为按钮绑定点击事件监听器
+        bt_cancel.setOnClickListener(mClickListener_cancel);
+        bt_confirm.setOnClickListener(mClickListener_confirm);
+    }
+}
--- app/src/main/java/com/lpc/smartlife/views/smartlife/RoomInfoActivity.java
+++ app/src/main/java/com/lpc/smartlife/views/smartlife/RoomInfoActivity.java
@@ -1,32 +1,57 @@
 package com.lpc.smartlife.views.smartlife;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.hxc.basemodule.BaseActivity;
 import com.lpc.smartlife.R;
+import com.lpc.smartlife.entity.DeviceList;
+import com.lpc.smartlife.entity.User;
+import com.lpc.smartlife.utils.CreateDialog;
+import com.lpc.smartlife.utils.Tools;
 
 public class RoomInfoActivity extends BaseActivity {
 
     ImageButton imageButtonBack;
     TextView tvUsersHome;
+    TextView tvEditUser;
+    TextView tvEditDeviceCount;
+    ImageView ivEditUser;
+    ImageView ivEditDeviceCount;
+    LinearLayout homeName;
+    LinearLayout deviceManager;
+    CreateDialog createDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room_info);
 
+        setImmersiveWindows();
         init();
     }
 
-    public void init(){
+    @Override
+    protected void onStart() {
+        super.onStart();
+        tvUsersHome.setText(User.user.getUserName());
+        tvEditUser.setText(User.user.getUserName());
+        tvEditDeviceCount.setText(DeviceList.deviceList.getCount() + "个设备");
+    }
+
+    public void init() {
         imageButtonBack = findViewById(R.id.imageButtonBack);
         tvUsersHome = findViewById(R.id.tvUsersHome);
+        tvEditUser = findViewById(R.id.tvEditUser);
+        tvEditDeviceCount = findViewById(R.id.tvEditDeviceCount);
+        ivEditUser = findViewById(R.id.ivEditUser);
+        ivEditDeviceCount = findViewById(R.id.ivEditDeviceCount);
+        homeName = findViewById(R.id.homeName);
+        deviceManager = findViewById(R.id.deviceManager);
 
         imageButtonBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,7 +59,41 @@ public class RoomInfoActivity extends BaseActivity {
                 finish();
             }
         });
+
+        homeName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog(view);
+            }
+        });
+
     }
+
+    public void showDialog(View view) {
+        createDialog = new CreateDialog(this, R.layout.edit_room_name_dialog, R.style.dialog, onClickListenerCancel, onClickListenerConfirm, tvEditUser.getText().toString().trim());
+        createDialog.show();
+    }
+
+    private View.OnClickListener onClickListenerCancel = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            createDialog.cancel();
+        }
+    };
+
+    private View.OnClickListener onClickListenerConfirm = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if (createDialog.et_home_name.getText().toString().trim().length() == 0)
+                Tools.displayToast(view.getContext(), "名称不能为空");
+            else {
+                User.user.setUserName(createDialog.et_home_name.getText().toString().trim());
+                tvUsersHome.setText(User.user.getUserName());
+                tvEditUser.setText(User.user.getUserName());
+                createDialog.cancel();
+            }
+        }
+    };
 
     @Override
     protected int getLayoutId() {
